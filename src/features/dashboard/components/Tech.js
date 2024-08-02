a3852e4b17e5c3e33c8cad25036aa636d9b62118
Olivier Paspuel <olivier.paspuel@gmail.com>
2024-08-02 03:15:11 -0500

    feat: dashboard view improved

--- src/features/dashboard/components/Tech.js
+++ src/features/dashboard/components/Tech.js
@@ -1,12 +1,13 @@
-import { Doughnut } from 'react-chartjs-2';
-import TitleCard from '../../../components/Cards/TitleCard';
+import { Doughnut } from "react-chartjs-2";
+import TitleCard from "../../../components/Cards/TitleCard";
+import Subtitle from "../../../components/Typography/Subtitle";
 
 function Tech() {
   const options = {
     responsive: true,
     plugins: {
       legend: {
-        position: 'top',
+        position: "top",
       },
     },
   };
@@ -16,25 +17,32 @@ function Tech() {
     labels,
     datasets: [
       {
-        label: 'Registros',
+        label: "Registros",
         data: [22, 78],
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.8)',
-          'rgba(54, 162, 235, 0.8)',
-        ],
-        borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-        ],
+        backgroundColor: ["rgba(255, 99, 132, 0.8)", "rgba(54, 162, 235, 0.8)"],
+        borderColor: ["rgba(255, 99, 132, 1)", "rgba(54, 162, 235, 1)"],
         borderWidth: 1,
-      }
+      },
     ],
   };
 
   return (
-    <TitleCard title={"Tecnología usada"}>
+    <div
+      className={
+        "h-3/4 card w-full p-6 bg-base-100 shadow-xl " +
+        "mt-6  text-left"
+      }
+    >
+      <Subtitle >
+        {"Representación de tecnologías en distintos paises"}
+      </Subtitle>
+      <div className="divider mt-2"></div>
+      <div className="flex justify-center items-center h-4/5">
+
+
       <Doughnut options={options} data={data} />
-    </TitleCard>
+      </div>
+    </div>
   );
 }
 
